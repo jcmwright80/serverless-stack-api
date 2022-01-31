d2190bb8f7aa484f7dd9baba486e230dcd096ad9
jcmwright80 <jcmwright80@gmail.com>
2022-01-31 00:29:21 +0000

    Building our react app

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,5 @@
 const config = {
+  STRIPE_KEY: "pk_test_51JXCHaKU1lAVlnYJ25Q9fxdSAGqdG1VoNrsADajQIwBkQ5KbP1DSQKKdKJmZNmJ5AtLFEtor0zf6fcUy0Z3ica2M00WDFlzcEF",
   MAX_ATTACHMENT_SIZE: 5000000,
     // Backend config
     s3: {
--- frontend/src/lib/awsLib.js
+++ frontend/src/lib/awsLib.js
@@ -9,3 +9,9 @@ export async function s3Upload(file) {
 
   return stored.key;
 }
+
+export async function s3Delete(file) {
+  const stored = await Storage.vault.remove(file);
+
+  return stored;
+}
